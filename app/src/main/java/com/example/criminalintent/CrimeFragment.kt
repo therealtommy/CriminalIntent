32443025c2ef2acb1d40427ed4925b60010edb4c
tommyallen <thomasclansi228@gmail.com>
2024-11-21 00:04:13 +0700

    Finalizing project, add reference for choose date

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -13,68 +13,54 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import java.util.Date
 import java.util.UUID
 
 private const val ARG_CRIME_ID = "crime_id"
 private const val TAG = "CrimeFragment"
-
-class CrimeFragment : Fragment()  {
-    private lateinit var titleField: EditText
-    private lateinit var crime: Crime
-    private lateinit var dateButton: Button
-    private lateinit var solvedCheckBox: CheckBox
-    public lateinit var button: Button
+private const val DIALOG_DATE = "DialogDate"
+private const val REQUEST_DATE = 0
 
 
-
-
+class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
+    private lateinit var crime : Crime
+    private lateinit var titleField : EditText
+    private lateinit var dateButton : Button
+    private lateinit var solvedCheckBox : CheckBox
     private val crimeDetailViewModel : CrimeDetailViewModel by lazy {
         ViewModelProvider(this).get(CrimeDetailViewModel::class.java)
     }
-    override fun onCreate(savedInstanceState:
-                          Bundle?) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+
         val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
         crimeDetailViewModel.loadCrime(crimeId)
-
     }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =
-            inflater.inflate(R.layout.fragment_crime,
-                container, false)
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
         titleField = view.findViewById(R.id.crime_title) as EditText
-        dateButton =
-            view.findViewById(R.id.crime_date) as Button
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-            solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
-        }
-
-
+        dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
         return view
     }
 
-    override fun onViewCreated(view: View,
-                               savedInstanceState: Bundle?) {
-        super.onViewCreated(view,
-            savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         crimeDetailViewModel.crimeLiveData.observe(
             viewLifecycleOwner,
-            Observer { crime ->
-                crime?.let {
-                    this.crime = crime
-                    updateUI()
-                }
+            Observer { crime -> crime?.let {
+                this.crime = crime
+                updateUI()
+            }
             })
     }
-
-
 
     override fun onStart() {
         super.onStart()
@@ -86,8 +72,7 @@ class CrimeFragment : Fragment()  {
                 count: Int,
                 after: Int
             ) {
-// Это пространство оставлено
-
+                // Это пространство оставлено пустым специально
             }
             override fun onTextChanged(
                 sequence: CharSequence?,
@@ -95,26 +80,27 @@ class CrimeFragment : Fragment()  {
                 before: Int,
                 count: Int
             ) {
-                crime.title =
-                    sequence.toString()
+                crime.title = sequence.toString()
             }
-            override fun
-                    afterTextChanged(sequence: Editable?) {
-// И это
+            override fun afterTextChanged(
+                sequence: Editable?
+            ) {
+                // И это
             }
         }
-
-
-
         titleField.addTextChangedListener(titleWatcher)
-
         solvedCheckBox.apply {
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
             }
         }
 
-
+        dateButton.setOnClickListener {
+            DatePickerFragment.newInstance(crime.date).apply {
+                setTargetFragment(this@CrimeFragment, REQUEST_DATE)
+                show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
+            }
+        }
     }
 
     override fun onStop() {
@@ -122,7 +108,10 @@ class CrimeFragment : Fragment()  {
         crimeDetailViewModel.saveCrime(crime)
     }
 
-
+    override fun onDateSelected(date: Date) {
+        crime.date = date
+        updateUI()
+    }
 
     private fun updateUI() {
         titleField.setText(crime.title)
@@ -131,22 +120,17 @@ class CrimeFragment : Fragment()  {
             isChecked = crime.isSolved
             jumpDrawablesToCurrentState()
         }
-
     }
 
-
     companion object {
-        fun newInstance(crimeId: UUID):
-                CrimeFragment {
+        fun newInstance(crimeId: UUID) : CrimeFragment {
             val args = Bundle().apply {
-                putSerializable(ARG_CRIME_ID,
-                    crimeId)
+                putSerializable(ARG_CRIME_ID, crimeId)
             }
             return CrimeFragment().apply {
                 arguments = args
             }
         }
     }
-
 
 }
