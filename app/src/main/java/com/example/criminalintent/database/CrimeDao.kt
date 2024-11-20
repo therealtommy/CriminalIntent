066b3660b54dd853a7c5791633ae6ee2656bce3a
tommyallen <thomasclansi228@gmail.com>
2024-11-20 21:08:30 +0700

    First queries for database

---
+++ app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,14 @@
+package com.example.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.criminalintent.Crime
+import java.util.UUID
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
+}
